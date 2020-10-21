252c73b98c627327b7c0d58503ea87ae0b2a9708
Md Shahriyar Alam <mdshahriyaralam552@gmail.com>
2020-10-22 05:51:08 +0600

    Added avatar

--- scripts/ui.js
+++ scripts/ui.js
@@ -11,9 +11,12 @@ class ChatUI{
         // console.log(this.list, data)
         const html = `
         <li class="list-group-item">
-            <span class="username">${data.author}</span>
-            <span class="message">${data.message}</span>
-            <div class="time">${dateFns.distanceInWordsToNow(data.created_at.toDate())} ago</div>
+            <img src="https://robohash.org/${data.author}?size=30x30&set=set4" alt="${data.author}" style="position: absolute; left: 5px; top: 50%; margin-top: -15px;">
+            <div style="margin-left: 25px;">
+                <span class="username">${data.author}</span>
+                <span class="message">${data.message}</span>
+                <div class="time">${dateFns.distanceInWordsToNow(data.created_at.toDate())} ago</div>
+            </div>
         </li>
         `;
         
